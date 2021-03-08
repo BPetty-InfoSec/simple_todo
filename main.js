703e4972057c378e9c0529294ba7bd2cc61b18c0
BPetty_InfoSec <bwpetty@protonmail.com>
2021-03-07 21:19:30 -0500

    add .js code

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// Project idea and code from https://www.w3schools.com/howto/howto_js_todolist.asp
+
+// First, create a "Close" button next to each item 
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for(i = 0; i < myNodeList.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7")
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+
+//click on the close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for(i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+// Create a new list item when clicking on the Add button
+function newElement() {
+    var li = document.createElement('li');
+    var inputValue = document.getElementById('inputTask').value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue=''){
+        alert("You must write something");
+    } else {
+        document.getElementById("myList").appendChild(li);
+    }
+    document.getElementById('inputTask').value = '';
+
+    var span = document.createElement('span');
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for(i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
